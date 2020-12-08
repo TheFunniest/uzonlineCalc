857e2cba63c3bab998f0cdf433fca5d2c49b133e
Doniyor Rajabov <donickuzb@gmail.com>
2020-12-08 23:14:27 +0500

    basic conf

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CalcForm from "./components/CalcForm";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <CalcForm />
+    </>
+  );
+};
+
+export default App;
