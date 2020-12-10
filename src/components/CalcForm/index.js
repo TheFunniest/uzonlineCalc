18bc030d36248b553fe418d34199fcc650fc61d2
Doniyor Rajabov <donickuzb@gmail.com>
2020-12-10 20:57:01 +0500

    add calculating

--- src/components/CalcForm/index.js
+++ src/components/CalcForm/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Button, Form, InputNumber } from "antd";
+import { AppContext } from "../../App";
 import "./styles.scss";
 import { ReactComponent as CpuIcon } from "../../img/cpu 1.svg";
 import { ReactComponent as RamIcon } from "../../img/ram 1.svg";
@@ -11,6 +12,21 @@ import BottomDrawer from "../BottomDrawer";
 
 const CalcForm = () => {
   const [visible, setVisible] = useState(false);
+  const [form] = Form.useForm();
+  const { tableData, setTableData } = useContext(AppContext);
+  const numberWithSpaces = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+  const uuid = () => {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  };
 
   return (
     <>
@@ -22,7 +38,7 @@ const CalcForm = () => {
           </p>
         </div>
         <div className="calc-card">
-          <Form className="calc-form">
+          <Form form={form} className="calc-form">
             <div className="calc-form__item">
               <div className="calc-form__label">
                 <CpuIcon />
@@ -39,9 +55,9 @@ const CalcForm = () => {
                 <RamIcon />
                 <span>RAM</span>
               </div>
-              <Form.Item>
+
+              <Form.Item name="ram">
                 <InputNumber
-                  name="ram"
                   type="number"
                   min={0}
                   className="calc-form__input"
@@ -55,10 +71,9 @@ const CalcForm = () => {
                 <SsdIcon />
                 <span>SSD</span>
               </div>
-              <Form.Item>
+              <Form.Item name="ssd">
                 <InputNumber
                   type="number"
-                  name="ssd"
                   min={0}
                   className="calc-form__input"
                 />
@@ -71,10 +86,9 @@ const CalcForm = () => {
                 <HardIcon />
                 <span>SAS - 15 K RPM</span>
               </div>
-              <Form.Item>
+              <Form.Item name="sas15k">
                 <InputNumber
                   type="number"
-                  name="sas15k"
                   min={0}
                   className="calc-form__input"
                 />
@@ -87,11 +101,10 @@ const CalcForm = () => {
                 <HardIcon />
                 <span>SAS - 10 K RPM</span>
               </div>
-              <Form.Item>
+              <Form.Item name="sas10k">
                 <InputNumber
                   type="number"
                   min={0}
-                  name="sas10k"
                   className="calc-form__input"
                 />
               </Form.Item>
@@ -103,10 +116,9 @@ const CalcForm = () => {
                 <HardIcon />
                 <span>SAS - 7,2 K RPM</span>
               </div>
-              <Form.Item>
+              <Form.Item name="sas7k">
                 <InputNumber
                   type="number"
-                  name="sas7k"
                   min={0}
                   className="calc-form__input"
                 />
@@ -119,10 +131,9 @@ const CalcForm = () => {
                 <WorldIcon />
                 <span>Интернет *</span>
               </div>
-              <Form.Item>
+              <Form.Item name="internet">
                 <InputNumber
                   type="number"
-                  name="internet"
                   min={0}
                   className="calc-form__input"
                 />
@@ -135,11 +146,10 @@ const CalcForm = () => {
                 <TasixIcon />
                 <span>TAS-IX **</span>
               </div>
-              <Form.Item>
+              <Form.Item name="tasix">
                 <InputNumber
                   type="number"
                   min={0}
-                  name="tasix"
                   className="calc-form__input"
                 />
               </Form.Item>
@@ -159,12 +169,73 @@ const CalcForm = () => {
             className="calc-btns__item add"
             htmlType="submit"
             onClick={() => {
+              form.validateFields().then((values) => {
+                form.resetFields();
+                const date = new Date();
+                const createdAt =
+                  date.getUTCDate() +
+                    "." +
+                    date.getUTCMonth() +
+                    "." +
+                    date.getUTCFullYear() +
+                    "  " +
+                    (date.getUTCHours() + 5) +
+                    ":" +
+                    (date.getUTCMinutes().toString().length <
+                  1
+                    ? "0" + date.getUTCMinutes()
+                    : date.getUTCMinutes());
+                const vcpu = values.vcpu ? values.vcpu * 71000 : 0;
+                const ram = values.ram ? values.ram * 10000 : 0;
+                const ssd = values.ssd ? values.ssd * 2200 : 0;
+                const sas15k = values.sas15k ? values.sas15k * 800 : 0;
+                const sas10k = values.sas10k ? values.sas10k * 600 : 0;
+                const sas7k = values.sas7k ? values.sas7k * 400 : 0;
+                const internet = values.internet ? values.internet : 0;
+                const tasix = values.tasix ? values.tasix : 0;
+                const overall =
+                  vcpu +
+                  ram +
+                  ssd +
+                  sas15k +
+                  sas10k +
+                  sas7k +
+                  internet +
+                  tasix +
+                  " сум";
+                const calculation = {
+                  key: uuid(),
+                  vcpu: numberWithSpaces(vcpu),
+                  ram: numberWithSpaces(ram),
+                  ssd: numberWithSpaces(ssd),
+                  sas15k: sas15k === 0 ? "-" : numberWithSpaces(sas15k),
+                  sas10k: sas10k === 0 ? "-" : numberWithSpaces(sas10k),
+                  sas7k: sas7k === 0 ? "-" : numberWithSpaces(sas7k),
+                  internet: numberWithSpaces(internet),
+                  tasix: numberWithSpaces(tasix),
+                  overall: numberWithSpaces(overall),
+                  createdAt,
+                };
+                const archive =
+                  JSON.parse(localStorage.getItem("calculations")) || [];
+                archive.push(calculation);
+                localStorage.setItem(
+                  "calculations",
+                  JSON.stringify([...archive])
+                );
+                setTableData([...tableData, calculation]);
+              });
               setVisible(true);
             }}
           >
             ДОБАВИТЬ
           </Button>
-          <Button className="calc-btns__item clear">ОЧИСТИТЬ </Button>
+          <Button
+            className="calc-btns__item clear"
+            onClick={() => form.resetFields()}
+          >
+            ОЧИСТИТЬ{" "}
+          </Button>
         </div>
       </div>
       <BottomDrawer visible={visible} onClose={() => setVisible(false)} />
