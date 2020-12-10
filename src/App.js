25eccfbeda83c99108fc03b6c9ccf3f5556e190a
Doniyor Rajabov <donickuzb@gmail.com>
2020-12-10 20:56:11 +0500

    created context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import CalcForm from "./components/CalcForm";
 import Header from "./components/Header";
 
+export const AppContext = createContext();
+
 const App = () => {
+  const [tableData, setTableData] = useState([]);
+  const [showDelete, setShowDelete] = useState(false);
+  const [deletingItem, setDeletingItem] = useState({});
+
   return (
-    <>
+    <AppContext.Provider
+      value={{
+        tableData,
+        setTableData,
+        showDelete,
+        setShowDelete,
+        deletingItem,
+        setDeletingItem,
+      }}
+    >
       <Header />
       <CalcForm />
-    </>
+    </AppContext.Provider>
   );
 };
 
